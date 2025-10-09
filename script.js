e673f4d79f89e7ff692993aad2bbd92cd12a0694
Torkel0825 <dtk000825@gmail.com>
2025-10-09 23:49:23 +0200

    Assingment 4

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+
+Create a meny for different options:
+    1. calculator
+    2. picture(frame) selector
+    3. rock, paper, scissors
+    4?
+
+have them appear in different boxes(div) on the page, but have them hidden initialy. 
+    or give them their own html file, up to you
+
+    
+
+*/
