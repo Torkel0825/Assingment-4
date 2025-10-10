7585975ccf356b8429b34c8c938725f96156a31d
Torkel0825 <dtk000825@gmail.com>
2025-10-10 12:38:46 +0200

    added a visual element to style and edit my website, no js yet

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ Create a meny for different options:
     1. calculator
     2. picture(frame) selector
     3. rock, paper, scissors
-    4?
+    4. list generator, with option for themes, and arrays containing objects that are randomized
+    5. a small section with fake produckts,
+        when the button is pressed, show a picture (big) and play a toot sound.wav
+    6. create a big section that showcases a background landscape ish place with flexbox
+    7?
 
 have them appear in different boxes(div) on the page, but have them hidden initialy. 
     or give them their own html file, up to you
