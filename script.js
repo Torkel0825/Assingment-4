d18d4e2215f68b017765a08dacdf5d7218934511
Torkel0825 <dtk000825@gmail.com>
2025-12-10 10:01:32 +0100

    bop

--- script.js
+++ script.js
@@ -56,4 +56,43 @@ function appendNumber(number) {
   ).value = `${previousInput} ${currentOperation} ${currentInput}`;
 }
 
-// function
+function appendOperation(operation) {
+  if (currentInput === "") return;
+  if (previousInput !== "") {
+    calculate();
+  }
+  currentOperation = operation;
+  previousInput = currentInput;
+  currentInput = "";
+  document.getElementById(
+    "display"
+  ).value = `${previousInput} ${currentOperation}`;
+}
+
+function calculate() {
+  if (previousInput === "" || currentInput === "") return;
+  let result;
+  let prev = parseFloat(previousInput);
+  let current = parseFloat(currentInput);
+
+  switch (currentOperation) {
+    case "+":
+      result = prev + current;
+      break;
+    case "-":
+      result = prev - current;
+      break;
+    case "*":
+      result = prev * current;
+      break;
+    case "/":
+      if (current === 0) {
+        alert("Cannot divide by zero");
+        return;
+      }
+      result = prev / current;
+      break;
+    default:
+      return;
+  }
+}
